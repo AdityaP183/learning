95d10f36bb25065f6b08fc5fe022c3db7ee3d19d
AdityaP183 <adityaprasad1828@gmail.com>
2025-10-13 21:34:26 +0530

    feat: implement user registration with bcrypt hashing and add Auth module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { RegisterDto } from "./dto/registerUser.dto";
+
+@Controller("auth")
+export class AuthController {
+	constructor(private readonly authService: AuthService) {}
+
+	@Post("register")
+	register(@Body() registerUserDto: RegisterDto) {
+		const response = this.authService.registerUser(registerUserDto);
+		return response;
+	}
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@nestjs/common";
+import { UserService } from "src/user/user.service";
+import { RegisterDto } from "./dto/registerUser.dto";
+import bcrypt from "bcrypt"
+
+@Injectable()
+export class AuthService {
+	constructor(private readonly userService: UserService) {}
+	async registerUser(registerUserDto: RegisterDto) {
+        const hashedPassword = await bcrypt.hash(registerUserDto.password, 10);
+
+		return this.userService.createUser({...registerUserDto, password: hashedPassword});
+	}
+}
