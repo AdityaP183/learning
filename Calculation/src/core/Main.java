ca2fbaeb9dd95a0c423bc658f40365fc876f1220
AdityaP183 <adityaprasad1828@gmail.com>
2026-04-19 13:45:57 +0530

    fix: assignment 1 Main.java

--- Calculation/src/core/Main.java
+++ Calculation/src/core/Main.java
@@ -10,21 +10,21 @@ public class Main {
 
 
 //        1. Factorial
-//        int val = sc.nextInt();
-//        System.out.print("Enter the number = ");
-//        System.out.println("Factorial value for "+val +" = "+ Calculation.factorial(val));
+        System.out.print("Enter the number = ");
+        int val = sc.nextInt();
+        System.out.println("Factorial value for "+val +" = "+ Calculation.factorial(val));
 
 //        2. Prime Number
-//        int val = sc.nextInt();
-//        System.out.print("Enter the number = ");
-//        System.out.println(val + (Calculation.isPrime(val) ? " is a prime number" : " is not a prime number"));
+        System.out.print("Enter the number = ");
+        int prime_val = sc.nextInt();
+        System.out.println(prime_val + (Calculation.isPrime(prime_val) ? " is a prime number" : " is not a prime number"));
 
 //        3. GCD
         System.out.print("Enter the first number = ");
         int val1 = sc.nextInt();
         System.out.print("Enter the second number = ");
         int val2 = sc.nextInt();
-        System.out.println("GCD of 12 and 18 = " + Calculation.gcd(val1, val2));
+        System.out.println("GCD of " + val1 + " and " + val2 + " = " + Calculation.gcd(val1, val2));
 
         sc.close();
     }
---
+++ Complex Numbers/src/ComplexDemo/Main.java
@@ -0,0 +1,4 @@
+package ComplexDemo;
+
+public class Main {
+}
